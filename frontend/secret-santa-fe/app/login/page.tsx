13470f339224a56056c82cc60e11433471b1dbb9
hasshk <hassan.sheikha@kambi.com>
2025-11-26 13:35:26 +0100

    manage user input for login

--- frontend/secret-santa-fe/app/login/page.tsx
+++ frontend/secret-santa-fe/app/login/page.tsx
@@ -3,11 +3,14 @@ import { Button, InputAdornment, TextField } from "@mui/material";
 import Cookies from 'js-cookie';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LockPersonIcon from '@mui/icons-material/LockPerson';
+import { useState } from "react";
 
 export default function LoginPage() {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <TextField id="username-field" label="Username" variant="standard" slotProps={{
+            <TextField id="username-field" label="Username" variant="standard" value={userName} onChange={(e) => setUserName(e.target.value)} slotProps={{
                 input: {
                     startAdornment: (
                         <InputAdornment position="start">
@@ -16,7 +19,7 @@ export default function LoginPage() {
                     ),
                 },
             }} />
-            <TextField id="password-field" label="Password" variant="standard" type="password" slotProps={{
+            <TextField id="password-field" label="Password" variant="standard" type="password" value={password} onChange={(e) => setPassword(e.target.value)} slotProps={{
                 input: {
                     startAdornment: (
                         <InputAdornment position="start">
@@ -25,14 +28,14 @@ export default function LoginPage() {
                     ),
                 },
             }} />
-            <Button variant="contained" color="primary" onClick={handleLogin} sx={{ marginTop: '20px', fontWeight: 'bold', backgroundColor: 'white', color: 'red', '&:hover': { backgroundColor: '#d12020ff' } }}>
+            <Button variant="contained" color="primary" onClick={() => handleLogin(userName, password)} sx={{ marginTop: '20px', fontWeight: 'bold', backgroundColor: 'white', color: 'red', '&:hover': { backgroundColor: '#d12020ff' } }}>
                 Login
             </Button>
         </div>
     );
 }
 
-function handleLogin() {
+function handleLogin(userName?: string, password?: string) {
     Cookies.set('authenticated', 'true');
     window.location.href = '/';
 }
