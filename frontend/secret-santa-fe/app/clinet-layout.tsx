7568e6869864ec64a84d345a311776b6a2d2f1e6
hasshk <hassan.sheikha@kambi.com>
2025-11-28 09:49:12 +0100

    disable page scrolling

--- frontend/secret-santa-fe/app/clinet-layout.tsx
+++ frontend/secret-santa-fe/app/clinet-layout.tsx
@@ -1,7 +1,15 @@
 'use client';
 import Snowfall from 'react-snowfall';
+import { useEffect } from 'react';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        const prev = document.body.style.overflow;
+        document.body.style.overflow = 'hidden'; // disable page scrolling
+        return () => {
+            document.body.style.overflow = prev; // restore on unmount
+        };
+    }, []);
     return (
         <div
             style={{
